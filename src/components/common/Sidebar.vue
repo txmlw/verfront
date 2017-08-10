4f49a784b78eeec8e6734de637a885b1f7689dc9
mrliang <284355163@qq.com>
2017-08-10 12:47:22 +0800

    修复翻页问题

--- src/components/common/Sidebar.vue
+++ src/components/common/Sidebar.vue
@@ -42,74 +42,74 @@
                                 title: '角色授权'
                             }
                         ]
-                    },
-                    {
-                        icon: 'el-icon-setting',
-                        index: '1',
-                        title: '设置',
-                        subs: [
-                            {
-                                index: 'svnst',
-                                title: 'svn设置'
-                            },
-                            {
-                                index: 'ftpst',
-                                title: 'ftp设置'
-                            },
-                            {
-                                index: 'dirst',
-                                title: '目录设置'
-                            }
-                        ]
-                    },
-                    {
-                        icon: 'el-icon-menu',
-                        index: '2',
-                        title: '表格',
-                        subs: [
-                            {
-                                index: 'basetable',
-                                title: '基础表格'
-                            },
-                            {
-                                index: 'vuetable',
-                                title: 'Vue表格组件'
-                            }
-                        ]
-                    },
-                    {
-                        icon: 'el-icon-date',
-                        index: '3',
-                        title: '表单',
-                        subs: [
-                            {
-                                index: 'baseform',
-                                title: '基本表单'
-                            },
-                            {
-                                index: 'vueeditor',
-                                title: '编辑器'
-                            },
-                            {
-                                index: 'markdown',
-                                title: 'markdown'
-                            },
-                            {
-                                index: 'upload',
-                                title: '文件上传'
-                            }
-                        ]
-                    },
-                    {
-                        icon: 'el-icon-star-on',
-                        index: 'basecharts',
-                        title: '图表'
-                    },
-                    {
-                        icon: 'el-icon-upload2',
-                        index: 'drag',
-                        title: '拖拽'
                     }
+//                    {
+//                        icon: 'el-icon-setting',
+//                        index: '1',
+//                        title: '设置',
+//                        subs: [
+//                            {
+//                                index: 'svnst',
+//                                title: 'svn设置'
+//                            },
+//                            {
+//                                index: 'ftpst',
+//                                title: 'ftp设置'
+//                            },
+//                            {
+//                                index: 'dirst',
+//                                title: '目录设置'
+//                            }
+//                        ]
+//                    },
+//                    {
+//                        icon: 'el-icon-menu',
+//                        index: '2',
+//                        title: '表格',
+//                        subs: [
+//                            {
+//                                index: 'basetable',
+//                                title: '基础表格'
+//                            },
+//                            {
+//                                index: 'vuetable',
+//                                title: 'Vue表格组件'
+//                            }
+//                        ]
+//                    },
+//                    {
+//                        icon: 'el-icon-date',
+//                        index: '3',
+//                        title: '表单',
+//                        subs: [
+//                            {
+//                                index: 'baseform',
+//                                title: '基本表单'
+//                            },
+//                            {
+//                                index: 'vueeditor',
+//                                title: '编辑器'
+//                            },
+//                            {
+//                                index: 'markdown',
+//                                title: 'markdown'
+//                            },
+//                            {
+//                                index: 'upload',
+//                                title: '文件上传'
+//                            }
+//                        ]
+//                    },
+//                    {
+//                        icon: 'el-icon-star-on',
+//                        index: 'basecharts',
+//                        title: '图表'
+//                    },
+//                    {
+//                        icon: 'el-icon-upload2',
+//                        index: 'drag',
+//                        title: '拖拽'
+//                    }
                 ]
             }
         },
